24774873180b41b4d3b72647e9861f19e3a70722
nbmehta <bhagavatibhai93@gmail.com>
2018-09-02 00:18:44 -0700

    Added repository for city and country

---
+++ ljprojects/src/main/java/com/ljproject/repository/CityRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ljproject.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Nitesh
+ *
+ */
+@Repository("cityRepository")
+public interface CityRepository {
+
+}
---
+++ ljprojects/src/main/java/com/ljproject/repository/CountryRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ljproject.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Nitesh
+ *
+ */
+@Repository("countryRepository")
+public interface CountryRepository {
+
+}
